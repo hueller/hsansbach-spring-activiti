86632c3d30c769abd7c4337f7f063c5b7ebea298
Matthias Hüller <matthias.hueller@googlemail.com>
2013-04-26 23:31:20 +0200

    Formatting.

--- spring-activiti-engine/src/main/java/de/hsansbach/wif/ebusiness/engine/SpelExpression.java
+++ spring-activiti-engine/src/main/java/de/hsansbach/wif/ebusiness/engine/SpelExpression.java
@@ -10,50 +10,53 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class SpelExpression implements Expression {
 
+	private static final long serialVersionUID = 1L;
+
 	private org.springframework.expression.Expression spelExpression;
-    
-    private SpelExpressionManager expressionManager;
 
-    public SpelExpression(String expressionText, SpelExpressionManager expressionManager) {
-        this.expressionManager = expressionManager;
-        spelExpression = expressionManager.expressionCache.get(expressionText);
-        if (spelExpression == null) {
-            try {
-                spelExpression = expressionManager.parser.parseExpression(expressionText, expressionManager.parserContext);
-            } catch (ParseException e) {
-                throw new ExpressionException("Error parsing " + expressionText, e);
-            }
-            expressionManager.expressionCache.put(expressionText, spelExpression);
-        }
-    }
+	private SpelExpressionManager expressionManager;
 
-    @Override
-    public Object getValue(VariableScope variableScope) {
-        EvaluationContext evaluationContext = createEvaluationContext(variableScope);
-        try {
-            Object value = spelExpression.getValue(evaluationContext);
-            return value;
-        } catch (EvaluationException e) {
-            throw new EvaluationException("Error evaluating [" + spelExpression.getExpressionString() + "]", e);
-        }
-    }
+	public SpelExpression(String expressionText, SpelExpressionManager expressionManager) {
+		this.expressionManager = expressionManager;
+		spelExpression = expressionManager.expressionCache.get(expressionText);
+		if (spelExpression == null) {
+			try {
+				spelExpression = expressionManager.parser.parseExpression(expressionText,
+						expressionManager.parserContext);
+			} catch (ParseException e) {
+				throw new ExpressionException("Error parsing " + expressionText, e);
+			}
+			expressionManager.expressionCache.put(expressionText, spelExpression);
+		}
+	}
 
-    @Override
-    public void setValue(Object value, VariableScope variableScope) {
-        EvaluationContext evaluationContext = createEvaluationContext(variableScope);
-        spelExpression.setValue(evaluationContext, value);
-    }
+	@Override
+	public Object getValue(VariableScope variableScope) {
+		EvaluationContext evaluationContext = createEvaluationContext(variableScope);
+		try {
+			Object value = spelExpression.getValue(evaluationContext);
+			return value;
+		} catch (EvaluationException e) {
+			throw new EvaluationException("Error evaluating [" + spelExpression.getExpressionString() + "]", e);
+		}
+	}
 
-    @Override
-    public String getExpressionText() {
-        return spelExpression.getExpressionString();
-    }
+	@Override
+	public void setValue(Object value, VariableScope variableScope) {
+		EvaluationContext evaluationContext = createEvaluationContext(variableScope);
+		spelExpression.setValue(evaluationContext, value);
+	}
 
-    private EvaluationContext createEvaluationContext(VariableScope variableScope) {
-        StandardEvaluationContext evaluationContext = new StandardEvaluationContext(variableScope);
-        evaluationContext.setBeanResolver(expressionManager.beanResolver);
-        evaluationContext.setVariables(variableScope.getVariables());
-        return evaluationContext;
-    }
+	@Override
+	public String getExpressionText() {
+		return spelExpression.getExpressionString();
+	}
+
+	private EvaluationContext createEvaluationContext(VariableScope variableScope) {
+		StandardEvaluationContext evaluationContext = new StandardEvaluationContext(variableScope);
+		evaluationContext.setBeanResolver(expressionManager.beanResolver);
+		evaluationContext.setVariables(variableScope.getVariables());
+		return evaluationContext;
+	}
 
 }
